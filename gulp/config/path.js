dcb326306c7571bf1dd936b4053bf78a1ef5604a
Maksym Korostelov <flying1519@gmail.com>
2023-03-03 16:43:56 +0200

    first common commit

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,39 +1,38 @@
-import * as nodePath from 'path';
-const rootFolder = nodePath.basename(nodePath.resolve());
+// Получаем имя папки проекта
+import * as nodePath from 'path'
 
-const buildFolder = `./dist`;
-const srcFolder = `./src`;
+const rootFolder = nodePath.basename(nodePath.resolve())
+const buildFolder = `./dist`
+const srcFolder = `./src`
 
 export const path = {
-    build: {
-        fonts: `${buildFolder}/fonts/`,
-        images: `${buildFolder}/img/`,
-        svg: `${buildFolder}/img/`,
-        js: `${buildFolder}/js/`,
-        css: `${buildFolder}/css/`,
-        html: `${buildFolder}/`,
-        files: `${buildFolder}/files/`
-    },
-    src: {
-        fonts: `${srcFolder}/fonts/**/*.woff`,
-        images: `${srcFolder}/img/**/*.{jpg,webp,jpeg,png,gif}`,
-        svg: `${srcFolder}/img/**/*.svg`,
-        js: `${srcFolder}/js/app.js`,
-        scss: `${srcFolder}/scss/style.scss`,
-        html: `${srcFolder}/*.html`,
-        files: `${srcFolder}/files/**/*.*`,
-    },
-    watch: {
-        images: `${srcFolder}/img/**/*.{jpg,webp,ico,svg,jpeg,png,gif}`,
-        svg: `${srcFolder}/img/**/*.svg`,
-        js: `${srcFolder}/js/**/*.js`,
-        scss: `${srcFolder}/scss/**/*.scss`,
-        html: `${srcFolder}/**/*.html`,
-        files: `${srcFolder}/files/**/*.*`,
-    },
-    clean: buildFolder,
-    buildFolder: buildFolder,
-    srcFolder: srcFolder,
-    rootFolder: rootFolder,
-    ftp: ``
+  build: {
+    js: `${buildFolder}/js/`,
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
+    images: `${buildFolder}/images/`,
+    fonts: `${buildFolder}/fonts/`,
+    static: `${buildFolder}/static/`,
+  },
+  src: {
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/images/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/images/**/*.svg`,
+    scss: `${srcFolder}/scss/style.scss`,
+    html: `${srcFolder}/*.html`,
+    static: `${srcFolder}/static/**/*.*`,
+    svgicons: `${srcFolder}/svgicons/*.svg`,
+  },
+  watch: {
+    js: `${srcFolder}/js/**/*.js`,
+    scss: `${srcFolder}/scss/**/*.scss`,
+    html: `${srcFolder}/**/*.html`,
+    images: `${srcFolder}/**/*.{jpg,jpeg,png,svg,gif,webp,ico}`,
+    static: `${srcFolder}/static/**/*.*`,
+  },
+  clean: buildFolder,
+  buildFolder: buildFolder,
+  srcFolder: srcFolder,
+  root: rootFolder,
+  ftp: ``, // Путь к нужной папке на удаленном сервере. gulp добавит имя папки проекта автоматически
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,53 @@
-//main module
-import gulp from "gulp";
-//path import 
-import { path } from "./gulp/config/path.js";
-//import global plugins
-import { plugins } from "./gulp/config/plugins.js";
+import gulp from 'gulp' // Основной модуль
+import { path } from './gulp/config/path.js' // Импорт путей
+import { plugins } from './gulp/config/plugins.js' // Импорт общих плагинов
 
-//global var
+// Импорт задач
+import { copy } from './gulp/tasks/copy.js'
+import { reset } from './gulp/tasks/reset.js'
+import { html } from './gulp/tasks/html.js'
+import { server } from './gulp/tasks/server.js'
+import { scss } from './gulp/tasks/scss.js'
+import { js } from './gulp/tasks/js.js'
+import { images } from './gulp/tasks/images.js'
+import { otfToTtf, ttfToWoff, fontStyle } from './gulp/tasks/fonts.js'
+import { svgSprive } from './gulp/tasks/svgSprive.js'
+import { zip } from './gulp/tasks/zip.js'
+import { ftp } from './gulp/tasks/ftp.js'
+
+// Передаем значения в глобальную переменную
 global.app = {
-    path: path,
-    gulp: gulp,
-    plugins: plugins
-};
+  isBuild: process.argv.includes('--build'),
+  isDev: !process.argv.includes('--build'),
+  path,
+  gulp,
+  plugins,
+}
 
-//tasks import
-import { reset } from "./gulp/tasks/reset.js";
-import { html } from "./gulp/tasks/html.js";
-import { scss } from "./gulp/tasks/scss.js";
-import { js } from "./gulp/tasks/js.js";
-import { images } from "./gulp/tasks/images.js";
-import { fonts } from "./gulp/tasks/fonts.js";
+// Наблюдатель за изменениями в файлах
+function watcher() {
+  gulp.watch(path.watch.static, copy)
+  gulp.watch(path.watch.html, html)
+  gulp.watch(path.watch.scss, scss)
+  gulp.watch(path.watch.js, js)
+  gulp.watch(path.watch.images, images)
+}
 
+// Последовательная обработка шрифтов
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontStyle)
+// Основные задачи
+const mainTasks = gulp.series(
+  fonts,
+  gulp.parallel(copy, html, scss, js, images)
+)
+// Построение сценариев выполнения задач
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
+const build = gulp.series(reset, mainTasks)
+const deployZIP = gulp.series(reset, mainTasks, zip)
+const deployFTP = gulp.series(reset, mainTasks, ftp)
 
-//watcher
-function watcher () {
-    gulp.watch(path.watch.html, html);
-    gulp.watch(path.watch.scss, scss);
-    gulp.watch(path.watch.js, js);
-    gulp.watch(path.watch.images, images);
-};
+// Выполнение сценария по умолчанию
+gulp.task('default', dev)
 
-const mainTasks = gulp.parallel(html, scss, js, images, fonts);
-//scenario building
-const dev = gulp.series(reset, mainTasks, watcher);
-
-//default scenario
-gulp.task('default', dev);
+// Экспорт сценариев
+export { dev, build, deployZIP, deployFTP, svgSprive }
