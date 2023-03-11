1faa1f116922257a0fddfdd7ee896697b9830bb0
Maksym Korostelov <flying1519@gmail.com>
2023-03-11 13:16:00 +0200

    develop renew

---
+++ gulp/tasks/js.js
@@ -0,0 +1,20 @@
+import webpack from "webpack-stream";
+import minify from "gulp-minify";
+
+export const js = () => {
+    return app.gulp.src(app.path.src.js)
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "JS",
+                message: "Error: <%= error.message %>"
+            })
+        ))
+        .pipe(webpack({
+            mode: 'development',
+            output: {
+                filename: 'app-p.js',
+            },
+        }))
+        .pipe(minify())
+        .pipe(app.gulp.dest(app.path.build.js));
+}
